ae43398b920b42bdcf9e7edcaff0197f195623db
Leszek Swirski <leszek@swirski.co.uk>
2014-08-20 15:23:38 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -1,8 +1,19 @@
-import github.daviesian.art.poly
+from tealight.art import *
+
+c = [(100,100), (50,200), (200,300), (270, 50), (270, 400), (290,20)]
+fill_polygon(c)
+
+color("red")
+#line(50,60,1000,60)
+#spot(287.89,60,5)
+print test_polygon(50,50,c)
 
 
-def foo():
-  foo
-  bar
-  
-foo()
+
+for i in range(0,500,3):
+  for j in range(0,500,3):
+    if test_polygon(i,j,c):
+      color("red")
+    else:
+      color("blue")
+    circle(i,j,0.2)    
