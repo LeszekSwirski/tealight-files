c9be0354f4ea5677ee0d533c4cfdb31b8a8c6f52
Leszek Swirski <leszek@swirski.co.uk>
2014-08-20 17:08:51 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -10,8 +10,8 @@ print test_polygon(270,50,c)
 
 
 
-for i in range(0,500,1):
-  for j in range(0,500,1):
+for i in range(0,500,4):
+  for j in range(0,500,4):
     if test_polygon(i,j,c):
       color("red")
     else:
