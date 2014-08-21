a7b98e6d3db8c5ff835f2615af1dcf99b9751cac
Leszek Swirski <leszek@swirski.co.uk>
2014-08-21 14:16:44 +0100

    Running art/traingle_anim.py

--- art/traingle_anim.py
+++ art/traingle_anim.py
@@ -30,10 +30,10 @@ def draw():
                         screen_height/2.,
                         max_size-size,
                         i*m_a)
-    color("hsl(0,100%," + str(round(max_l - l)) + "%)")
+    color("hsl(0,100%," + str(round(l)) + "%)")
     fill_polygon(tri)
   
-    color("hsl(0,100%," + str(round(max_l - l - min_l)) + "%)")
+    color("hsl(0,100%," + str(round(l - min_l)) + "%)")
     polygon(tri)
     
 line_width(3)
