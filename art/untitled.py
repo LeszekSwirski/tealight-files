d64584c0285d861723e26e75ec61916aa2711eea
Leszek Swirski <leszek@swirski.co.uk>
2014-08-21 11:47:20 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -10,10 +10,10 @@ def triangle(x,y,size,angle=0):
   for i in range(0,3):
     theta = angle + i*2*pi/3
     pts.append((x + size*sin(theta),
-                y + size*sin(theta)))
+                y + size*cos(theta)))
     
   print pts
-  polygon([(100.0, 100.0), (143.301270189, 143.301270189), (56.6987298108, 56.6987298108)])
+  polygon(pts)
   
   
   
