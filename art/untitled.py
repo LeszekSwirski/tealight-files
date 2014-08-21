2a74de0d41e6f7888a2780be301617b453e1d966
Leszek Swirski <leszek@swirski.co.uk>
2014-08-21 11:45:55 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -8,7 +8,7 @@ from math import sin, cos, pi
 def triangle(x,y,size,angle=0):
   pts = []
   for i in range(0,3):
-    theta = angle + i*2*pi/3.0
+    theta = angle + i*2*pi/3
     pts.append((x + size*sin(theta),
                 y + size*sin(theta)))
     
