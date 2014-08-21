d6932a4836f95124ff7c048a7c45b6664e202743
Leszek Swirski <leszek@swirski.co.uk>
2014-08-21 14:15:54 +0100

    Running art/traingle_anim.py

--- art/traingle_anim.py
+++ art/traingle_anim.py
@@ -22,14 +22,14 @@ def draw():
   color("black")
   box(0,0,screen_width,screen_height)
   
-  for i in range(0, 0+num_triangles):
+  for i in range(5, 5+num_triangles):
     size = min_size + (max_size - min_size) * float(i) / num_triangles
     l = min_l + (max_l - min_l) * float(i) / num_triangles
     
     tri = make_triangle(screen_width/2.,
                         screen_height/2.,
                         max_size-size,
-                        i*m_a + (i%2)*pi)
+                        i*m_a)
     color("hsl(0,100%," + str(round(max_l - l)) + "%)")
     fill_polygon(tri)
   
