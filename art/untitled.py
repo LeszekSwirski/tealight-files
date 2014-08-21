26c5b58a18d98218680c15cacd4421648dcbbdaf
Leszek Swirski <leszek@swirski.co.uk>
2014-08-21 10:59:54 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -1,6 +1,7 @@
-import tealight
+from tealight.utils import github_load
 
-art = tealight.art
-from art import rectangle
+ian = github_load("daviesian", "logo", "lib")
+
+ian.lib_func()
 
 box(0,0,100,100)
