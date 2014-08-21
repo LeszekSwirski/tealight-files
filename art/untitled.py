562a39e0172b82fec31ec172c12336b4f0c396f0
Leszek Swirski <leszek@swirski.co.uk>
2014-08-21 14:00:51 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -2,44 +2,31 @@ from tealight.art import box,line_width,polygon,color,fill_polygon,clear,screen_
 from math import sin, cos, pi
 from tealight.utils import sleep
 
-
-
-def triangle(x,y,size,angle=0):
+def make_triangle(x,y,size,angle=0):
   pts = []
   for i in range(0,3):
     theta = angle + i*2*pi/3
     pts.append((x + size*sin(theta),
                 y + size*cos(theta)))
-    
-  polygon(pts)
-
-def fill_triangle(x,y,size,angle=0):
-  pts = []
-  for i in range(0,3):
-    theta = angle + i*2*pi/3
-    pts.append((x + size*sin(theta),
-                y + size*cos(theta)))
-    
-  fill_polygon(pts)
+  return pts;
   
 
 m_a = 0
 
 line_width(3)
 def handle_frame():
-  global m_a
+  m_a = sin(now())
   
   color("white")
   box(0,0,screen_width,screen_height)
   
   for i in range(0,25,1):
+    tri = make_triangle(200,200,200-i*6,2*i*m_a)
     color("hsl(0,100%," + str(i*4) + "%)")
-    fill_triangle(200,200,200-i*6,2*i*m_a)
+    fill_polygon(tri)
   
     color("hsl(0,100%," + str(i*4-20) + "%)")
-    triangle(200,200,200-i*6,2*i*m_a)
-    
-  m_a += 0.002
+    draw_polygon(tri)
   
   sleep(30)
     
