f0de84e88f6b6d2d68f1103219cd413278fc6230
Leszek Swirski <leszek@swirski.co.uk>
2014-08-21 11:52:24 +0100

    Running art/untitled.py

--- art/untitled.py
+++ art/untitled.py
@@ -24,9 +24,10 @@ def fill_triangle(x,y,size,angle=0):
   fill_polygon(pts)
   
   
-
+line_width(5)
 for i in range(0,50):
   color("hsl(0,100%," + str(i*2) + "%)")
   fill_triangle(200,200,200-i*2,i*0.5)
+
   color("hsl(0,100%," + str(i*2-20) + "%)")
   triangle(200,200,200-i*2,i*0.5)
